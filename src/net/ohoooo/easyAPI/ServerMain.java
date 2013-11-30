9c1161145c0d38acc610677ea8016f7153ebb023
oho-s <oho-s@klab.com>
2013-11-30 21:33:23 +0900

    First Commit

---
+++ src/net/ohoooo/easyAPI/ServerMain.java
@@ -0,0 +1,25 @@
+/*
+ * Created on 2013/11/30
+ *
+ * To change the template for this generated file go to
+ * Window>Preferences>Java>Code Generation>Code and Comments
+ */
+package net.ohoooo.easyAPI;
+
+/**
+ * @author Suguru Oho
+ *
+ * To change the template for this generated type comment go to
+ * Window>Preferences>Java>Code Generation>Code and Comments
+ */
+public class ServerMain {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
